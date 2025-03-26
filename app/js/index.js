59d0a0c0a3ddd676e8157502147b475bc296b181
fernando <fernandoat90@gmail.com>
2025-03-26 02:20:56 -0300

    mas modificaciones en proyectos

--- app/js/index.js
+++ app/js/index.js
@@ -2,12 +2,12 @@ function alterFont(){
     const saludo=document.getElementById('saludo');
     
         saludo.addEventListener('mouseover', function(){
-                saludo.style.fontSize="50px";
+                saludo.style.fontSize="60px";
                 saludo.style.transition="1s";
         });
 
         saludo.addEventListener('mouseout', function(){
-            saludo.style.fontSize="35px";
+            saludo.style.fontSize="45px";
             saludo.style.transition="1s";
         });
 }
